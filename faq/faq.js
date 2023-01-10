559cce70b5654b8190427f9e4c5e07dbfcd690d4
sheblair <66489366+sheblair@users.noreply.github.com>
2023-01-10 16:33:40 -0500

    fixed links and added js to update to current year

--- faq/faq.js
+++ faq/faq.js
@@ -1,7 +1,4 @@
-// update years to current years
-const reviewHeading = document.querySelector(".reviews-heading")
 const copyright = document.querySelector(".copyright-paragraph")
 const currentYear = new Date().getFullYear();
 
-reviewHeading.innerText = `LaCrue Bottling Co. October ${currentYear - 1}`
 copyright.innerText = `Copyright ${currentYear}  ⎮  Unplugged`;
